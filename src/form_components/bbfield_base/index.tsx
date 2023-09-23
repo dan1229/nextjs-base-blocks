1b3f086d36f0d2ebd3857e80265ce6f5a902f4f3
Daniel Nazarian <danielnazarian@outlook.com>
2023-09-23 02:26:03 -0400

    added value prop support to bbfieldbase

--- src/form_components/bbfield_base/index.tsx
+++ src/form_components/bbfield_base/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import InputWrapper from '../input_wrapper'
-import type { IPropsBBBaseForm, TBBFieldBaseTypes } from '../../types'
+import React from 'react';
+import InputWrapper from '../input_wrapper';
+import type { IPropsBBBaseForm, TBBFieldBaseTypes } from '../../types';
 
 /**
  * PROPS
- * 
+ *
  * @param {TBBFieldBaseTypes=} type - Type of input. Think 'text' or 'textarea'.
  */
 export interface IPropsBBFieldBase {
@@ -14,20 +14,29 @@ export interface IPropsBBFieldBase {
 /**
  * BBFIELD BASE
  */
-export default function BBFieldBase (Props: IPropsBBFieldBase & IPropsBBBaseForm): React.ReactElement {
-  const { register, fieldName, type, required = false, autocomplete, onChange } = Props
+export default function BBFieldBase(Props: IPropsBBFieldBase & IPropsBBBaseForm): React.ReactElement {
+  const { register, fieldName, type, required = false, autocomplete, onChange, value } = Props;
 
   const getAutoComplete = (): string => {
-    if (autocomplete) return autocomplete
-    return fieldName
-  }
+    if (autocomplete) return autocomplete;
+    return fieldName;
+  };
 
   /**
    * RENDER
    */
   return (
     <InputWrapper {...Props}>
-      <input className="form-control" id={fieldName} type={type} autoComplete={getAutoComplete()} required={required} onChange={onChange} {...register} />
+      <input
+        className="form-control"
+        id={fieldName}
+        type={type}
+        autoComplete={getAutoComplete()}
+        required={required}
+        onChange={onChange}
+        value={value}
+        {...register}
+      />
     </InputWrapper>
-  )
+  );
 }
