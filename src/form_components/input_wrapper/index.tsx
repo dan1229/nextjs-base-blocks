1d72515d54efa3c8435e7a050c010ced57c623f3
Daniel Nazarian <danielnazarian@outlook.com>
2023-09-22 23:57:45 -0400

    input wrapper fix?

--- src/form_components/input_wrapper/index.tsx
+++ src/form_components/input_wrapper/index.tsx
@@ -1,10 +1,10 @@
-import classnames from "classnames";
-import React, { Children } from "react";
-import type { IPropsBBBaseForm } from "../../types";
+import classnames from 'classnames';
+import React, { Children } from 'react';
+import type { IPropsBBBaseForm } from '../../types';
 
 /**
  * IProps
- * 
+ *
  * @props {React.ReactNode} children - Children to render.
  */
 export interface IPropsInputWrapper {
@@ -14,27 +14,27 @@ export interface IPropsInputWrapper {
 /**
  * INPUT WRAPPER
  */
-export default function InputWrapper (props: IPropsInputWrapper & IPropsBBBaseForm): React.ReactElement {
-  const { label, className, fieldName } = props
-
+export default function InputWrapper(props: IPropsInputWrapper & IPropsBBBaseForm): React.ReactElement {
+  const { label, className, fieldName } = props;
 
   const getLabel = (): string => {
-    if (!!label && label.length) return label
-    const fieldNameRes = fieldName.includes('_') ? fieldName.replace('_', ' ') : fieldName
-    return fieldNameRes.charAt(0).toUpperCase() + fieldNameRes.slice(1)
-  }
+    if (!!label && label.length) return label;
+    const fieldNameRes = fieldName.includes('_') ? fieldName.replace('_', ' ') : fieldName;
+    return fieldNameRes.charAt(0).toUpperCase() + fieldNameRes.slice(1);
+  };
 
-  const {children, ...childProps} = props
-  
+  const { children, ...childProps } = props;
+
   /**
    * RENDER
    */
-    return (<div className={classnames('form-group', className)}>
-    <label htmlFor={fieldName}>{getLabel()}</label>
-    {
-      Children.map(children, (child) => {
-        return React.cloneElement(child, {...childProps})
-      })
-    }
-  </div>)
+  return (
+    <div className={classnames('form-group', className)}>
+      <label htmlFor={fieldName}>{getLabel()}</label>
+      {React.Children.map(children, (child) => {
+        console.log('child', child);
+        return React.cloneElement(child, { props: childProps });
+      })}
+    </div>
+  );
 }
