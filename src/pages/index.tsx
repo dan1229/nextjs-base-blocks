97a1be2f20ca6ecc009c35201ba0f5d27fc8f6a1
Daniel Nazarian <danielnazarian@outlook.com>
2023-10-30 17:16:57 -0400

    prettier settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
 const IndexPage = () => {
-    return (
+  return (
+    <div>
+      <div>
+        <h1>NextJS Base Blocks</h1>
+        <p>Base blocks for NextJS projects.</p>
+        {/* TODO fill out description */}
+        {/* Add link to Github repo */}
+      </div>
+      <div>
+        <h3>BBAlert</h3>
         <div>
-            <h1>Hello, world!</h1>
+          <p>TODO</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default IndexPage;
