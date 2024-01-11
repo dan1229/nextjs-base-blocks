570cec0bbc3366e09b26b9627d138a2ff695bdcc
Daniel Nazarian <danielnazarian@outlook.com>
2024-01-10 22:09:00 -0500

    Fix/alert field error ref undefined bug (#37)
    * Fix AlertsFieldErrors bug related to ref
    
    * cleanup

--- src/form_components/helpers/alerts/alerts_field_errors/index.tsx
+++ src/form_components/helpers/alerts/alerts_field_errors/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BBAlert from '../../../../bbalert';
-import type { FieldErrors, Ref } from 'react-hook-form';
+import type { FieldError, FieldErrors, FieldErrorsImpl, Merge, Ref } from 'react-hook-form';
 
 /**
  * IProps
@@ -25,12 +25,18 @@ export function AlertsFieldErrors(props: IPropsAlertsFieldErrors): React.ReactEl
     return <></>;
   }
 
-  const getErrorByType = (type: string | undefined) => {
+  const getErrorByType = (type: string | undefined, ref: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | Ref | undefined) => {
     if (!type) {
       return 'Invalid';
     }
     if (type === 'required') {
       return `Field is required`;
+    }
+    if (type === 'minLength') {
+      return `Field must have a minimum length`;
+    }
+    if (type === 'maxLength') {
+      return `Field must have a maximum length`;
     }
     return `Invalid - ${type}`;
   };
@@ -46,12 +52,11 @@ export function AlertsFieldErrors(props: IPropsAlertsFieldErrors): React.ReactEl
         const { message, type, ref } = fieldError;
         let finalMessage = message?.toString();
         try {
-          finalMessage = `Error - ${type} '${(ref as Ref).name.replace(/_/g, ' ').replace(/-/g, ' ')}'`;
+          finalMessage = `Error - ${type} '${(ref as Ref)?.name?.replace(/_/g, ' ').replace(/-/g, ' ')}'`;
         } catch (error) {
           console.error(error);
         }
-
-        finalMessage = finalMessage?.length ? finalMessage : getErrorByType(type?.toString());
+        finalMessage = finalMessage?.length ? finalMessage : getErrorByType(type?.toString(), ref);
 
         return (
           <BBAlert key={`${keyFieldError}-${Date.now()}`} variant="danger">
