2a4bce649d313b4024dd7aa1efee316e17979792
Daniel Nazarian <danielnazarian@outlook.com>
2024-04-21 00:24:57 -0400

    some styling

--- src/bbcollapsible/index.tsx
+++ src/bbcollapsible/index.tsx
@@ -1,88 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, Children, cloneElement } from 'react';
 import BBCard from '../bbcard';
 import styles from './styles.module.scss';
 import { IPropsBBCard } from 'src/bbcard';
 
 /**
- * PROPS
- *
- * @param {React.ReactNode | React.ReactNode[]} children - The text to display
- * @param {TBBCardColorBackground=} color - the color of the text
- * @param {TBBCardElevation} elevation - the elevation of the card
- * @param {string=} className - Any class name to add
- * @param {() => void=} onClick - Function to call when clicked
- * @param {string=} href - URL for the link
- * @param {TBBTextColor=} hrefColor - The color of the link
- * @param {boolean=} noBorder - Whether to remove the border
- */
-export interface IPropsBBCollapsible {
-  children: React.ReactNode | React.ReactNode[];
-  //   colorBackground?: TBBCardColorBackground;
-  //   colorBorder?: TBBCardColorBorder;
-  //   elevation?: TBBCardElevation;
-  //   className?: string;
-  //   onClick?: () => void;
-  //   href?: string;
-  //   hrefColor?: TBBTextColor;
-  //   noBorder?: boolean;
-}
-
-/**
  * BBCollapsible
+ * Renders a collapsible component with a header and body content.
  */
-const BBCollapsible = (props: IPropsBBCard) => {
+const BBCollapsible = ({ children, ...props }: IPropsBBCard) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   const toggleExpand = () => setIsExpanded(!isExpanded);
 
+  const header = Children.toArray(children).find((child: any) => child.type.displayName === 'Header');
+  const content = Children.toArray(children).find((child: any) => child.type.displayName === 'Content');
+
   return (
-    <BBCard>
-      <BBCard.Header onClick={toggleExpand}>{props.children}</BBCard.Header>
-      {isExpanded && <BBCard.Body>{props.children}</BBCard.Body>}
+    <BBCard {...props}>
+      {cloneElement(header as React.ReactElement, { onClick: toggleExpand, isExpanded })}
+      {isExpanded && content}
     </BBCard>
   );
 };
 
 /**
- * BBCollapsible.Header
- *
- * @param {React.ReactNode | React.ReactNode[]} children - The text to display
+ * PROPS
+ * @param {React.ReactNode | React.ReactNode[]} children - The children components, specifically Header and Content for the collapsible
  * @param {string=} className - Any class name to add
- * @param {() => void=} onClick - Function to call when clicked
- * @param {boolean=} noPadding - Whether to remove the padding
+ * @param {boolean=} noPadding - Whether to remove padding from the content
+ * @param {boolean=} isExpanded - Whether the content is expanded
  */
-interface IPropsBBCollapsibleHeader {
+interface IPropsBBCollapsibleSection {
   children: React.ReactNode | React.ReactNode[];
   className?: string;
-  onClick?: () => void;
   noPadding?: boolean;
+  isExpanded?: boolean;
 }
 
-const Header = (props: IPropsBBCollapsibleHeader) => {
-  return <BBCard.Header {...props}>{props.children}</BBCard.Header>;
+/**
+ * BBCollapsible.Header
+ */
+const Header = ({ children, isExpanded, ...props }: IPropsBBCollapsibleSection) => {
+  return (
+    <BBCard.Header {...props} className={styles.mainCollapsibleHeader}>
+      {children}
+      <div className={styles.containerArrow}>{isExpanded ? '▲' : '▼'}</div>
+    </BBCard.Header>
+  );
 };
 Header.displayName = 'Header';
-BBCollapsible.Header = Header;
 
 /**
  * BBCollapsible.Content
- *
- * @param {React.ReactNode | React.ReactNode[]} children - The text to display
- * @param {string=} className - Any class name to add
- * @param {() => void=} onClick - Function to call when clicked
- * @param {boolean=} noPadding - Whether to remove the padding
  */
-interface IPropsBBCollapsibleContent {
-  children: React.ReactNode | React.ReactNode[];
-  className?: string;
-  onClick?: () => void;
-  noPadding?: boolean;
-}
-
-const Content = (props: IPropsBBCollapsibleContent) => {
-  return <BBCard.Body {...props}>{props.children}</BBCard.Body>;
+const Content = ({ children, ...props }: IPropsBBCollapsibleSection) => {
+  return <BBCard.Body {...props}>{children}</BBCard.Body>;
 };
 Content.displayName = 'Content';
+
+// Assigning Header and Content as static properties of BBCollapsible
+BBCollapsible.Header = Header;
 BBCollapsible.Content = Content;
 
 export default BBCollapsible;
