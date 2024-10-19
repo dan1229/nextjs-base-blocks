918183f447fa1334edf713af6e88fc373e9023d2
Daniel Nazarian <danielnazarian@outlook.com>
2024-10-19 00:10:57 -0400

    Refactor BBNavbar component: Remove unused useEffect import

--- src/bbnavbar/index.tsx
+++ src/bbnavbar/index.tsx
@@ -3,7 +3,7 @@
 import classNames from 'classnames';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import BBText from '../bbtext';
 import useOutsideClick from '../utils/hooks/UseOutsideClick';
