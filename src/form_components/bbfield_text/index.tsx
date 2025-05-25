34bf526719ef4c3a5e4b85eb1fbc5065737cd3b8
Daniel Nazarian <danielnazarian@outlook.com>
2025-05-25 17:29:52 -0400

    Remove unused style prop from BBFieldText component for cleaner code.

--- src/form_components/bbfield_text/index.tsx
+++ src/form_components/bbfield_text/index.tsx
@@ -34,7 +34,6 @@ export default function BBFieldText(Props: IPropsBBFieldText & IPropsBBBaseForm)
     placeholder: placeholder,
     onChange: onChange,
     value: value,
-    style: undefined,
   };
 
   /**
