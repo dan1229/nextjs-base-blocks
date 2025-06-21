523325b0474840b337fa96e5e5730c8989c1fd1a
Daniel Nazarian <danielnazarian@outlook.com>
2025-06-21 13:25:37 -0400

    Add BBFieldDropdown component and enhance form control select styles
    - Introduced BBFieldDropdown to the form components demo page with state management for options.
    - Updated styles for form control select elements to improve appearance and usability, including custom dropdown styling and padding adjustments.

--- src/pages/form-components/index.tsx
+++ src/pages/form-components/index.tsx
@@ -5,9 +5,11 @@ import DemoComponent from '../../demo_components/demo_component';
 import BBFieldCheckbox from '../../form_components/bbfield_checkbox';
 import BBFieldFile from '../../form_components/bbfield_file';
 import BBFieldText from '../../form_components/bbfield_text';
+import BBFieldDropdown from '../../form_components/bbfield_dropdown';
 import type { IPropsBBFieldCheckbox } from '../../form_components/bbfield_checkbox';
 import type { IPropsBBFieldText } from '../../form_components/bbfield_text';
 import type { IPropsBBBaseForm } from '../../types';
+import { IPropsBBFieldDropdown } from '../../form_components/bbfield_dropdown';
 
 /**
  * FORM COMPONENTS DEMO PAGE
@@ -45,6 +47,21 @@ const FormComponentsPage = () => {
     label: 'Demo File Upload',
     className: '',
     onChange: () => {},
+    size: 'md',
+  });
+
+  // BB Field Dropdown
+  const [stateBBFieldDropdown, setStateBBFieldDropdown] = useState<IPropsBBFieldDropdown & IPropsBBBaseForm>({
+    fieldName: 'demo-dropdown',
+    required: false,
+    label: 'Demo Dropdown',
+    className: '',
+    onChange: () => {},
+    options: [
+      { value: '1', label: 'Option 1' },
+      { value: '2', label: 'Option 2' },
+      { value: '3', label: 'Option 3' },
+    ],
     size: 'md',
   });
 
@@ -123,6 +140,13 @@ const FormComponentsPage = () => {
           child={<BBFieldFile {...stateBBFieldFile} />}
           stateObject={stateBBFieldFile}
           setStateObject={setStateBBFieldFile}
+        />
+
+        <DemoComponent
+          name="BBFieldDropdown"
+          child={<BBFieldDropdown {...stateBBFieldDropdown} />}
+          stateObject={stateBBFieldDropdown}
+          setStateObject={setStateBBFieldDropdown}
         />
 
         {/* TODO not working for some reason */}
